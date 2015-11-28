e5f052b0ed6523d37cde99eabf558476a1a9afc5
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-11-28 13:41:53 +0100

    Correction de divers bugs et ajout de fonctionnalités manquantes

--- src/framework/Jeu.cpp
+++ src/framework/Jeu.cpp
@@ -10,7 +10,19 @@ Jeu::Jeu() {}
 
 bool Jeu::ajoutSystemeAffichage(Afficheur *a)
 {
+    if(a == nullptr)
+        return false;
+
     liste_affichage.push_back(a);
+    return true;
+}
+
+bool Jeu::ajoutJoueur(Joueur *j)
+{
+    if(j == nullptr)
+        return false;
+
+    joueurs.push_back(j);
     return true;
 }
 
@@ -18,6 +30,11 @@ bool Jeu::ajoutSystemeAffichage(Afficheur *a)
 Jeu::~Jeu()
 {
     for(vector<Afficheur *>::size_type i = 0; i < liste_affichage.size(); i++)
+    {
+        delete liste_affichage[i];
+    }
+
+    for(vector<Joueur *>::size_type i = 0; i < joueurs.size(); i++)
     {
         delete liste_affichage[i];
     }
--- src/framework/Jeu.hpp
+++ src/framework/Jeu.hpp
@@ -5,18 +5,24 @@
 #include <vector>
 
 class Afficheur;
+class Joueur;
 
 class Jeu
 {
+protected:
+
+    std::vector<Joueur *> joueurs;
     std::vector<Afficheur *> liste_affichage;
 
 public:
 
     Jeu();
 
+    virtual void demarrer() = 0;
     virtual void lancerPartie() = 0;
     virtual void arret() = 0;
 
+    bool ajoutJoueur(Joueur *j);
     bool ajoutSystemeAffichage(Afficheur *a);
 
     virtual ~Jeu();
