bb8675df566d01621d2b4a84f4521b6850fb2b5c
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-01 17:00:53 +0100

    Ajout de commentaire de documentation dans l'afficheur

--- src/framework/Afficheur.hpp
+++ src/framework/Afficheur.hpp
@@ -1,14 +1,31 @@
-
 #ifndef AFFICHEUR_HPP
 #define AFFICHEUR_HPP
 
-
+/**
+*   @class Afficheur
+*
+*   Class abstraite qui gère l'affichage
+*
+*/
 class Afficheur
 {
 
 public:
 
+    /**
+    *   @fn virtual void afficherMenu()
+    *
+    *   Afficher le menu principal
+    *
+    */
     virtual void afficherMenu() = 0;
+
+    /**
+    *   @fn virtual void afficherJeu()
+    *
+    *   Afficher l'état du jeu
+    *
+    */
     virtual void afficherJeu() = 0;
 
     virtual ~Afficheur();
