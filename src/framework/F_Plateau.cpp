64bf2f06a862994a7ef21c45ccbf8c3f207e5786
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-04 10:45:16 +0100

    Ajout du plateau, non fin et à tester dans la demo du jeu du serpent

---
+++ src/framework/F_Plateau.cpp
@@ -0,0 +1,65 @@
+
+
+#include "F_Plateau.hpp"
+
+
+static F_Plateau *singleton = nullptr;
+
+F_Plateau::F_Plateau(int nbCases,bool isProcedural)
+{
+    if(isProcedural)
+        generationProcedural();
+    else
+        generationClassique();
+}
+
+F_Plateau::~F_Plateau()
+{
+    /// @todo effacer les cases
+    // Empty
+}
+
+void F_Plateau::init(int nbCases, bool isProcedural)
+{
+    if(singleton == nullptr)
+        singleton = new F_Plateau(nbCases,isProcedural);
+}
+
+F_Plateau * getInstance()
+{
+    return singleton;
+}
+
+
+void F_Plateau::detruire()
+{
+    delete singleton;
+}
+
+
+void F_Plateau::generationClassique()
+{
+    /// @todo Génération du plateau par défaut
+}
+
+
+void F_Plateau::generationProcedural()
+{
+    /// @todo Génération procédural du plateau
+}
+
+
+F_Case& F_Plateau::operator [](unsigned int index)
+{
+    return *(this->cases.at(index));
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/framework/F_Plateau.hpp
@@ -0,0 +1,37 @@
+
+#ifndef PLATEAU_HPP
+#define PLATEAU_HPP
+
+#include <vector>
+
+class F_Case;
+
+/// @todo Faire la génération classique et procédurale du plateau
+/**
+*   @class F_Plateau
+*
+*   Singleton plateau
+*
+*/
+class F_Plateau
+{
+    std::vector<F_Case *> cases;
+
+    F_Plateau(int nbCases, bool isProcedural=false);
+    F_Plateau(F_Plateau& p);
+    F_Plateau& operator =(F_Plateau p);
+
+    ~F_Plateau();
+
+public:
+
+    void init(int nbCases, bool isProcedural=false);
+    F_Plateau * getInstance();
+    void generationClassique();     // Définir le plateau spécifié dans le sujet
+    void generationProcedural();    // Permet de générer un plateau aléatoirement
+    void detruire();
+
+    F_Case& operator [](unsigned int index);
+};
+
+#endif // PLATEAU_HPP
