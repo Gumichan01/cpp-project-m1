992b79cb5093e04b905f54b4cba6a9d445e4b9d2
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-08 14:18:07 +0100

    Génération classique avec les echelles

--- src/framework/F_Plateau.cpp
+++ src/framework/F_Plateau.cpp
@@ -1,11 +1,13 @@
 
 
+#include "F_Case.hpp"
 #include "F_Plateau.hpp"
 
 
 static F_Plateau *singleton = nullptr;
 
 F_Plateau::F_Plateau(int nbCases,bool isProcedural)
+    : cases(nbCases)
 {
     if(isProcedural)
         generationProcedural();
@@ -17,8 +19,8 @@ F_Plateau::~F_Plateau()
 {
     for(std::vector<F_Case *>::size_type i = 0; i != cases.size(); i++)
     {
-        delete enemies[i];
-        enemies.erase(cases.begin() + i);
+        delete cases[i];
+        cases.erase(cases.begin() + i);
         i--;
     }
 }
@@ -43,7 +45,47 @@ void F_Plateau::detruire()
 
 void F_Plateau::generationClassique()
 {
+    // 10 échelles et 10 serpents
     /// @todo Génération du plateau par défaut
+    for(std::vector<F_Case *>::size_type i = 0; i != cases.size(); i++)
+    {
+        switch(i)
+        {
+            case 2  :   cases[i] = new F_Case(TYPE_CASE::SAUT,18);
+                        break;
+
+            case 8  :   cases[i] = new F_Case(TYPE_CASE::SAUT,50);
+                        break;
+
+            case 10 :   cases[i] = new F_Case(TYPE_CASE::SAUT,30);
+                        break;
+
+            case 26 :   cases[i] = new F_Case(TYPE_CASE::SAUT,74);
+                        break;
+
+            case 40 :   cases[i] = new F_Case(TYPE_CASE::SAUT,60);
+                        break;
+
+            case 44 :   cases[i] = new F_Case(TYPE_CASE::SAUT,64);
+                        break;
+
+            case 52 :   cases[i] = new F_Case(TYPE_CASE::SAUT,70);
+                        break;
+
+            case 68 :   cases[i] = new F_Case(TYPE_CASE::SAUT,94);
+                        break;
+
+            case 78 :   cases[i] = new F_Case(TYPE_CASE::SAUT,100);
+                        break;
+
+            case 84 :   cases[i] = new F_Case(TYPE_CASE::SAUT,96);
+                        break;
+
+            default :   cases[i] = new F_Case(TYPE_CASE::NORMAL);
+                        break;
+        }
+    }
+
 }
 
 
@@ -57,13 +99,4 @@ F_Case& F_Plateau::operator [](unsigned int index)
 {
     return *(this->cases.at(index));
 }
-
-
-
-
-
-
-
-
-
 
