7586ec2de755127fd0d972918500c2e6157c9f23
celia-Nassima <celia_saidani@yahoo.fr>
2015-12-14 14:15:29 +0100

    Joueur humain

---
+++ src/demo/JoueurHumain.hpp
@@ -0,0 +1,19 @@
+#ifndef JOUEURHUMAIN_HPP_INCLUDED
+#define JOUEURHUMAIN_HPP_INCLUDED
+#include "../framework/F_Joueur.hpp"
+#include <iostream>
+
+class Joueurhumain : public F_Joueur{
+public:
+Joueurhumain (std::string n,int nbPions, int sc = 0);
+void jouer();
+bool gagner();
+
+
+private:
+bool passe_tour;
+
+virtual ~Joueurhumain();
+};
+
+#endif // JOUEURHUMAIN_HPP_INCLUDED
