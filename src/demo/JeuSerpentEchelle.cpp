36da7672ca44b5eb7dc21b8808878bebd55a3690
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-14 14:47:13 +0100

    Jeu Serpent et echelle: classe de jeu fait

---
+++ src/demo/JeuSerpentEchelle.cpp
@@ -0,0 +1,33 @@
+
+#include "JeuSerpentEchelle.hpp"
+
+
+JeuSerpentEchelle::JeuSerpentEchelle()
+    : F_Jeu()
+{
+    // Vide
+}
+
+
+JeuSerpentEchelle::~JeuSerpentEchelle()
+{
+    // Vide
+}
+
+
+void JeuSerpentEchelle::demarrer()
+{
+    /// @todo Ajout joueurs
+    /// @todo Ajout afficheur
+}
+
+void JeuSerpentEchelle::lancerPartie()
+{
+    /// @todo
+}
+
+void JeuSerpentEchelle::arret()
+{
+    /// @todo
+}
+
---
+++ src/demo/JeuSerpentEchelle.hpp
@@ -0,0 +1,31 @@
+#ifndef JEU_SERPENT_ECHELLE_HPP
+#define JEU_SERPENT_ECHELLE_HPP
+
+
+#include "../framework/F_Jeu.hpp"
+
+
+class JeuSerpentEchelle : public F_Jeu
+{
+
+public:
+
+    JeuSerpentEchelle();
+
+    virtual void demarrer();
+    virtual void lancerPartie();
+    virtual void arret();
+
+    virtual ~JeuSerpentEchelle();
+};
+
+
+
+
+
+
+
+
+
+#endif // JEU_SERPENT_ECHELLE_HPP
+
