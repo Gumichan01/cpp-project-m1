586ee5642d9b61f8b2fe9873dd536c1c72adbe91
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-14 14:30:54 +0100

    Commentaires

--- src/demo/JoueurHumain.cpp
+++ src/demo/JoueurHumain.cpp
@@ -17,20 +17,25 @@ Joueurhumain::Joueurhumain(std::string n,int nbPions, int sc)
 
 void Joueurhumain::jouer()
 {
-
     if(passe_tour)
     {
         passe_tour = false;
     }
     else
     {
+        // Lancer les dés
         int des= rand() %6 + 1;
         int position = pions[0].getPosition() + des;
 
+        // Récuperer le plateau pour intéragir avec
         F_Plateau * plateau = F_Plateau::getInstance();
+
+        // Avoir la cas où le joueur va atterrir
         F_Case& case_suivante = plateau->operator[](position);
 
+        // Le joueur quite la case courant
         case_suivante.enleverPion(pions[0].getPosition());
+
 
         if(!case_suivante.estVide())
         {
@@ -39,12 +44,16 @@ void Joueurhumain::jouer()
         }
         else
         {
+            // Selon le type de la case, effectuer une action donnée
             switch(case_suivante.getType())
             {
-            case SAUT :
+            case SAUT : // Echelle ou serpent
             {
                 pions[0].setPosition(case_suivante.getSautCase());
-
+                /*
+                    Faire un saut vers la case pointée par
+                    l'echelle ou le serpent
+                */
                 plateau->operator[](case_suivante.getSautCase()).ajoutPion(pions[0]);
             }
             break;
@@ -60,7 +69,7 @@ void Joueurhumain::jouer()
             case PASSE:
             {
                 pions[0].setPosition(position);
-                passe_tour=true;
+                passe_tour = true;
             }
             break;
 
@@ -70,13 +79,11 @@ void Joueurhumain::jouer()
 
             }
         }
-
     }
 }
 
 bool Joueurhumain::gagner()
 {
-
     for(F_Pion p : pions)
     {
         if(p.getPosition() != 100)
