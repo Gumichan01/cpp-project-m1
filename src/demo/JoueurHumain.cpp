c5d5a97731ecb8adc128f0e797d3698e0ae55c6c
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-12-14 14:18:31 +0100

    Formattage du code

--- src/demo/JoueurHumain.cpp
+++ src/demo/JoueurHumain.cpp
@@ -3,54 +3,72 @@
 #include "../framework/F_Plateau.hpp"
 #include "../framework/F_Case.hpp"
 
-
 #include <iostream>
 #include<stdlib.h>
 #include<time.h>
 
 
-Joueurhumain::Joueurhumain(std::string n,int nbPions, int sc):F_Joueur(n,nbPions,sc),passe_tour(false){}
+Joueurhumain::Joueurhumain(std::string n,int nbPions, int sc)
+    : F_Joueur(n,nbPions,sc), passe_tour(false)
 
-void Joueurhumain::jouer(){
-
-if(passe_tour)
 {
-    passe_tour = false;
+    // Vide
 }
-else
-{
-    int des= rand() %6 + 1;
-    int position = pions[0].getPosition() + des;
 
-    F_Plateau * plateau = F_Plateau::getInstance();
-    F_Case& case_courant = plateau->operator[](position);
+void Joueurhumain::jouer()
+{
+
+    if(passe_tour)
+    {
+        passe_tour = false;
+    }
+    else
+    {
+        int des= rand() %6 + 1;
+        int position = pions[0].getPosition() + des;
+
+        F_Plateau * plateau = F_Plateau::getInstance();
+        F_Case& case_courant = plateau->operator[](position);
 
 /// @todo enlever le pion de la case précédente
-case_courant.enleverPion(pions[0].getPosition());
+        case_courant.enleverPion(pions[0].getPosition());
 /// @todo retour à la case départ si la case d'arrivée est occupée
 
-    switch(case_courant.getType())
-    {
-    case SAUT : {
-                    pions[0].setPosition(case_courant.getSautCase());
+        switch(case_courant.getType())
+        {
+        case SAUT :
+        {
+            pions[0].setPosition(case_courant.getSautCase());
 
-                    plateau->operator[](case_courant.getSautCase()).ajoutPion(pions[0]);
-                } break;
+            plateau->operator[](case_courant.getSautCase()).ajoutPion(pions[0]);
+        }
+        break;
 
-    case REJOUER:   {pions[0].setPosition(position);
-                    case_courant.ajoutPion(pions[0]); jouer();} break;
+        case REJOUER:
+        {
+            pions[0].setPosition(position);
+            case_courant.ajoutPion(pions[0]);
+            jouer();
+        }
+        break;
 
-    case PASSE:  {pions[0].setPosition(position);passe_tour=true;} break;
+        case PASSE:
+        {
+            pions[0].setPosition(position);
+            passe_tour=true;
+        }
+        break;
 
-    default : pions[0].setPosition(position); break ;
+        default :
+            pions[0].setPosition(position);
+            break ;
 
+        }
     }
-
-
-}
 }
 
-bool Joueurhumain::gagner(){
+bool Joueurhumain::gagner()
+{
 
     for(F_Pion p : pions)
     {
@@ -59,3 +77,4 @@ bool Joueurhumain::gagner(){
     }
     return true;
 }
+
--- src/demo/JoueurHumain.hpp
+++ src/demo/JoueurHumain.hpp
@@ -1,19 +1,21 @@
 #ifndef JOUEURHUMAIN_HPP_INCLUDED
 #define JOUEURHUMAIN_HPP_INCLUDED
+
 #include "../framework/F_Joueur.hpp"
 #include <iostream>
 
-class Joueurhumain : public F_Joueur{
+class Joueurhumain : public F_Joueur
+{
 public:
-Joueurhumain (std::string n,int nbPions, int sc = 0);
-void jouer();
-bool gagner();
+    Joueurhumain (std::string n,int nbPions, int sc = 0);
+    void jouer();
+    bool gagner();
 
 
 private:
-bool passe_tour;
+    bool passe_tour;
 
-virtual ~Joueurhumain();
+    virtual ~Joueurhumain();
 };
 
 #endif // JOUEURHUMAIN_HPP_INCLUDED
